e2797742824b873eadc0d6a007034b8a56094e7a
Nurulla Ibadov <ibadnurulla@gmail.com>
2024-10-22 05:28:22 +0400

    Abstractions folder

---
+++ MyProject/Group.cs
@@ -0,0 +1,58 @@
+﻿public class Group
+{
+    public string GroupNo { get; set; }
+    public int StudentLimit { get; set; }
+    private Student[] Students;
+    private int currentStudentCount = 0;
+
+    public Group(string groupNo, int studentLimit)
+    {
+        if (studentLimit < 5 || studentLimit > 18)
+        {
+            throw new Exception("Student limit must be between 5 and 18.");
+        }
+
+        if (!CheckGroupNo(groupNo))
+        {
+            throw new Exception("Group number must consist of 2 uppercase letters followed by 3 digits.");
+        }
+
+        GroupNo = groupNo;
+        StudentLimit = studentLimit;
+        Students = new Student[StudentLimit];
+    }
+
+    public bool CheckGroupNo(string groupNo)
+    {
+        if (groupNo.Length == 5 &&
+            groupNo.Take(2).All(char.IsUpper) &&
+            groupNo.Skip(2).All(char.IsDigit))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void AddStudent(Student student)
+    {
+        if (currentStudentCount < StudentLimit)
+        {
+            Students[currentStudentCount++] = student;
+        }
+        else
+        {
+            throw new Exception("Cannot add more students, limit reached.");
+        }
+    }
+
+    public Student GetStudent(int? id)
+    {
+        if (id == null) return null;
+        return Students.FirstOrDefault(s => s != null && s.Id == id);
+    }
+
+    public Student[] GetAllStudents()
+    {
+        return Students.Where(s => s != null).ToArray();
+    }
+}
---
+++ MyProject/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            User user1 = new User("Ali Veli", "ali@example.com", "Password123");
+            user1.ShowInfo();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Student student1 = new Student("Nurulla Ibadov", 100);
+        Student student2 = new Student("Jesse Jackson", 20);
+
+        student1.StudentInfo();
+        student2.StudentInfo();
+
+        try
+        {
+            Group group1 = new Group("AB107", 10);
+
+            group1.AddStudent(student1);
+            group1.AddStudent(student2);
+
+            Student retrievedStudent = group1.GetStudent(student1.Id);
+            retrievedStudent.StudentInfo();
+
+            foreach (var student in group1.GetAllStudents())
+            {
+                student.StudentInfo();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
